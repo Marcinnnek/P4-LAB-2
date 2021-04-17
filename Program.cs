33ba1de8658955389eca474f9c7cc0524fa38216
Marcin Zuziak <mz048257@student.ath.edu.pl>
2021-04-17 17:12:18 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P4_LAB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
